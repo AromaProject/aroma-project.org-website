540cd14aaa75e0c175c25f441123a7b27d6a7ad6
hb <hb@aroma-project.org>
2017-01-04 12:51:21 -0800

    ROBUSTNESS: Throw error if 'invalid input found on input connection'

--- templates/R/utils.R
+++ templates/R/utils.R
@@ -1,8 +1,25 @@
 readLines2 <- function(file, ..., warn=FALSE, encoding=getOption("encoding")) {
-  con <- file(file, open="r", encoding=encoding)
+  ## IMPORTANT: Must use open="rb" for input encoding to work.
+  ## If one uses open="r", there will be an error! /HB 2017-01-04
+  con <- file(file, open="rb", encoding=encoding)
   on.exit(close(con))
 
-  body <- readLines(con, warn=warn, ...)
+  tryCatch({
+    body <- readLines(con, warn=warn, ...)
+  }, warning = function(w) {
+    msg <- conditionMessage(w)
+    pattern <- gettextf("invalid input found on input connection")
+    if (any(grepl(pattern, msg))) {
+      body <- readLines(file, warn=FALSE, encoding = encoding, ...)
+      mprint(con)
+      msg <- sprintf("ENCODING ERROR: %s\n", msg)
+      msg <- sprintf("%sInput encoding used: %s\n", msg, sQuote(encoding))
+      lines <- sprintf("%3d: %s", seq_along(body), sQuote(body))
+      msg <- sprintf("%sFile content parsed:\n%s\n", msg, paste(lines, collapse = "\n"))
+      throw(msg)
+    }
+    signalCondition(w)
+  })
   Encoding(body) <- encoding
 
   body
