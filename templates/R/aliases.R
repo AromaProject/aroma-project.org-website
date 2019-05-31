f2082ffd66fc1ec46e74d86960b3cf29c98b957c
Henrik Bengtsson <hb@aroma-project.org>
2019-05-31 11:47:30 -0700

    More HTTP -> HTTPS updates

--- templates/R/aliases.R
+++ templates/R/aliases.R
@@ -1,33 +1,33 @@
 chipTypeData <- function(chipType, filename) {
-  url <- sprintf("http://aroma-project.org/data/annotationData/chipTypes/%s/%s", chipType, filename)
+  url <- sprintf("https://aroma-project.org/data/annotationData/chipTypes/%s/%s", chipType, filename)
   sprintf("[%s](%s)", filename, url)
 } # chipTypeData()
 
 
 cran <- function(name) {
-  printf("[%s](http://cran.r-project.org/package=%s)", name, name)
+  printf("[%s](https://cran.r-project.org/package=%s)", name, name)
 }
 
 bioc <- function(name) {
   if (grepl("^(pd|org|BSgenome)[.]", name) || grepl("([.]db|[.]db0|cdf|probe)$", name)) {
-    printf("[%s](http://www.bioconductor.org/packages/release/data/annotation/html/%s.html)", name, name)
+    printf("[%s](https://www.bioconductor.org/packages/release/data/annotation/html/%s.html)", name, name)
   } else {
-    printf("[%s](http://www.bioconductor.org/packages/release/bioc/html/%s.html)", name, name)
+    printf("[%s](https://www.bioconductor.org/packages/release/bioc/html/%s.html)", name, name)
   }
 }
 
 pmid <- function(id) {
-  printf("[%s](http://www.ncbi.nlm.nih.gov/pubmed/%s)", id, id)
+  printf("[%s](https://www.ncbi.nlm.nih.gov/pubmed/%s)", id, id)
 }
 
 pcmid <- function(id) {
-  printf("[%s](http://www.ncbi.nlm.nih.gov/pmc/articles/%s/)", id, id)
+  printf("[%s](https://www.ncbi.nlm.nih.gov/pmc/articles/%s/)", id, id)
 }
 
 geo <- function(id) {
-  printf("[%s](http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=%s)", id, id)
+  printf("[%s](https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=%s)", id, id)
 }
 
 arrayexpress <- function(id) {
-  printf("[%s](http://www.ebi.ac.uk/microarray-as/ae/browse.html?keywords=%s)", id, id)
+  printf("[%s](https://www.ebi.ac.uk/microarray-as/ae/browse.html?keywords=%s)", id, id)
 }
